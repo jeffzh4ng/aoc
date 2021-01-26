2a9ef45ea3d5ecf1cca40b7b120720497eafd495
Jeff Zhang <zhang.jeffreyd@gmail.com>
2021-01-26 17:07:47 -0500

    Improve 2020-3 in Rust

--- 2020/rust/src/bin/3/main.rs
+++ 2020/rust/src/bin/3/main.rs
@@ -2,7 +2,16 @@ use std::{error::Error, fs};
 
 struct Slope {
     dx: usize,
-    dy: usize
+    dy: usize,
+}
+
+impl From<&(usize, usize)> for Slope {
+    fn from(slope_tuple: &(usize, usize)) -> Self {
+        Slope {
+            dx: slope_tuple.0,
+            dy: slope_tuple.1,
+        }
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -14,21 +23,18 @@ fn main() -> Result<(), Box<dyn Error>> {
         .map(|line| line.chars().collect())
         .collect();
 
-    let slope_tuples = [(1, 1), (3,1), (5,1), (7,1), (1,2)];
+    let slope_tuples = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
 
     let slopes: Vec<Slope> = slope_tuples
         .iter()
-        .map(|slope_tuple| Slope {
-            dx: slope_tuple.0,
-            dy: slope_tuple.1
-        })
+        .map(|slope_tuple: &(usize, usize)| Slope::from(slope_tuple))
         .collect();
 
-    let product = slopes
-        .iter()
-        .fold(1, |acc: u64, cur| acc * calc_num_of_trees_hit(cur.dx, cur.dy, &hill_grid));
+    let slopes_product = slopes.iter().fold(1, |acc: u64, cur| {
+        acc * calc_num_of_trees_hit(cur.dx, cur.dy, &hill_grid)
+    });
 
-    dbg!(product);
+    dbg!(slopes_product);
 
     Ok(())
 }
@@ -36,12 +42,13 @@ fn main() -> Result<(), Box<dyn Error>> {
 fn calc_num_of_trees_hit(dx: usize, dy: usize, hill_grid: &Vec<Vec<char>>) -> u64 {
     let mut trees_hit = 0;
     let mut r = dy;
-    let mut c  = dx;
+    let mut c = dx;
 
     // should we make hill_grid's type a sized Vec so compiler knows hill_grid[0]? if hill_grid[0] DNE is panics
     while r < hill_grid.len() {
-        if c >= hill_grid[0].len() {
-            c %= hill_grid[0].len();
+        let hill_width = hill_grid[0].len();
+        if c >= hill_width {
+            c %= hill_width;
         }
 
         if hill_grid[r][c] == '#' {
@@ -51,8 +58,6 @@ fn calc_num_of_trees_hit(dx: usize, dy: usize, hill_grid: &Vec<Vec<char>>) -> u6
         r += dy;
         c += dx;
     }
-
-
 
     trees_hit
 }
